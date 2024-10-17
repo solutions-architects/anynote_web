77454b6d55bf5abbf4037a70eed30205a9d7c279
Илья Воробьев <ilya7050@gmail.com>
2024-10-17 13:23:15 +0700

    Rename interface to navbar
    Fix minor bug.
    Add Link component for contact us button.

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -3,6 +3,7 @@ import Logo from "../../ui/components/Logo/Logo.tsx"
 import Button from "../../ui/components/Button/Button.tsx"
 import landing from "../../ui/assets/icons/landing.png"
 import {useNavigate} from "react-router-dom";
+import { Link } from "react-router-dom"
 function Landing() {
     const navigate = useNavigate();
     const handleButtonClick = (): void => {
@@ -27,23 +28,23 @@ function Landing() {
                         your notes and synchronizes them among multiple platforms. For free.</div>
                     <div className="landing__buttons-container">
                         <Button onClick={handleButtonClick} type="primary"
-                                className="landing__buttons--primary">
+                                className="landing__buttons-container--primary">
                             Download for Windows</Button>
                         <Button onClick={handleButtonClick} type="secondary"
-                                className="landing__buttons--secondary">
+                                className="landing__buttons-container--secondary">
                             Download for Android
                         </Button>
                         <Button onClick={handleButtonClick} type="secondary"
-                                className="landing__buttons--secondary">
+                                className="landing__buttons-container--secondary">
                             Download for Android
                         </Button>
                     </div>
                 </div>
                 </div>
                 <div className="landing__content landing__content--right">
-                    <div className="landing__interface">
-                        <div className="landing__interface-text">Contact us</div>
-                        <Button onClick={handleSignInButton} type="primary" className="landing__interface-width">
+                    <div className="landing__navbar">
+                        <Link className="landing__navbar-text" to={''}>Contact us</Link>
+                        <Button onClick={handleSignInButton} type="primary" className="landing__navbar-width">
                             Sign in</Button>
                     </div>
                     <img src={landing} alt="Landing" className="landing__main-image-size"/>
